d54c406b4030e984eb7037279765aa8831dcfbdf
mayuresh1301 <62095050+mayuresh1301@users.noreply.github.com>
2024-05-30 18:42:07 -0400

    Fixed errors and added routes and functions in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Login from './pages/login'
+import Register from './pages/register'
+import ProtectedRoutes from './components/ProtectedRoutes'
+import Home from './pages/home'
+import NotFound from './pages/NotFound'
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
+
 function App() {
  
 
   return (
-    <>
-   
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path = "/"
+          element = {
+            <ProtectedRoutes>
+              <Home />
+            </ProtectedRoutes>
+        
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -53,3 +53,4 @@ function ProtectedRoutes({children}) {
     return isAuthorized ? children : <Navigate to="/login" />;
 }
 
+export default ProtectedRoutes;
